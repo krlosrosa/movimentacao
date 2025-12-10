479cdce868110511dfbb8e6e429dc00e41bc7bb2
carlos <robertorsilva.krlos@gmail.com>
2025-12-10 04:15:32 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+'use client';
+import { Button } from "@/_shared/_components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const handleStartProcess = () => {
+    router.push('/demanda');
+  }
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
+      <div>
+        <Button onClick={handleStartProcess}>
+          Iniciar Processo de Movimentação
+        </Button>
+      </div>
     </div>
   );
 }
